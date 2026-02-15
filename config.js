83cba19d41c6d6ea0108ef227f198ae7afc9e79a
vivirenremoto <gafeman@gmail.com>
2026-02-15 11:28:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
   // Reemplaza estos valores por los de tu proyecto Supabase.
-  supabaseUrl: 'https://TU-PROYECTO.supabase.co',
-  supabaseAnonKey: 'TU_SUPABASE_ANON_KEY'
+  supabaseUrl: 'https://fclhjbwhgtgwsieacgtb.supabase.co',
+  supabaseAnonKey: 'sb_publishable__4xv1faJc3YSYjN87l23Og_b1727O6N'
 };
